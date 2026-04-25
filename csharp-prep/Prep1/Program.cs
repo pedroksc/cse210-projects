5cb43969ac5c838fbd94165db2966128226caa2b
Pedro Camargo <pedro.k.s.c.sud@gmail.com>
2026-04-25 19:10:01 -0400

    Finished Prep 1 and two

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
+//     Console.Write("What is your favorite color? ");
+// string color = Console.ReadLine();
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string stringGrade = Console.ReadLine();
+        int intGrade = int.Parse(stringGrade);
+
+        string letter = "";
+
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, try again next time.");
+        }
     }
 }
