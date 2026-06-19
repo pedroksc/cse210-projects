16aa8a5b7d69c395d3a2887edca2e94ba4b82327
Pedro Camargo <pedro.k.s.c.sud@gmail.com>
2026-06-18 21:10:05 -0600

    Shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
+
+
+List<Shape> shapes = new List<Shape>();
+
+shapes.Add(new Square("Red", 4));
+shapes.Add(new Rectangle("Blue", 6, 3));
+shapes.Add(new Circle("Green", 5));
+shapes.Add(new Square("Yellow", 2.5));
+
+Console.WriteLine("Paper Shape Area Report");
+Console.WriteLine("=======================");
+
+foreach (Shape shape in shapes)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+    string color = shape.GetColor();
+    double area = shape.GetArea();
+    Console.WriteLine($"Color: {color,-7} Area: {area:F2}");
 }
